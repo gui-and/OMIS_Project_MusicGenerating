7ddbd8afd7cd2f404b86ee204932bfb76a83a206
dajaj <aurele.dousselin@centrale-marseille.fr>
2016-11-27 19:28:46 +0100

    Turn a list of MIDI files into a 3D tensor (sample_size,max_MidiFile_length,128)

---
+++ to_test_sample.py
@@ -0,0 +1,17 @@
+import mido_encodage as me
+
+def test_sample(midiFileList):
+	sampleList = []
+	max_len = 0
+	counter = 0
+	for midiFile in midiFileList:
+		encodedMidi = me.note_egal_vect(midiFile)
+		max_len = max(max_len,len(encodedMidi))
+		sampleList.append(encodedMidi)
+		counter+=1
+	sampleArray = np.zeros((counter,max_len,128),np.int)
+	for mid in range(counter):
+		for tempo in range(len(sampleList[mid])):
+			for note in range(128):
+				sampleArray[mid,tempo,note] = sampleList[mid][tempo][note]
+	return sampleArray
