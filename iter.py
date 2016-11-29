deacee7af12e9bc2726aac742a8a8b49bece7c1e
dajaj <aurele.dousselin@centrale-marseille.fr>
2016-11-29 19:54:08 +0100

    File looping
    * Iter an action recursively through a directory
    
    * fixup! Iter an action recursively through a directory
    
    * fixup! fixup! Iter an action recursively through a directory
    
    * fixup! fixup! fixup! Iter an action recursively through a directory

--- iter.py
+++ iter.py
@@ -1,29 +1,24 @@
 import mido_encodage as me
 import os
 
-def splitChannels(midiName,midiSource):
-	outDir = 'channels/' + midiSource
-	if not os.path.exists(outDir):
-	  os.makedirs(outDir)
-	
-	outDir = outDir + '/' + midiName[:-4]
-	if not os.path.exists(outDir):
-	  os.makedirs(outDir)
-	
-	midiFile = 'MIDI/' + midiSource + '/' + midiName
-	
-	channels,metas=me.parseMidi(midiFile)
-	me.saveMidiList(channels,outDir+'/chan_')
+def scanDir(path="./",directory="."):
+	midiSample = []
+	for file in os.listdir(path+directory):
+		if os.path.isdir(path+directory+"/"+file):
+			scanDir(path+directory+"/",file,action)
+		else:
+			addMidiToList(path+directory+"/"+file,midiSample)
+	return midiSample
+
+def addMidiToList(midiFile,midiList):
+	try:
+		channels,metas=me.parseMidi(midiFile)
+		for channel in channels:
+			midiList.append(channel)
+	except:
+		os.remove(midiFile)
+		print(midiFile+" REMOVED")
+
 
 if __name__ == "__main__":
-	for sourceDir in os.listdir('MIDI'):
-		print(sourceDir)
-		for midiFile in os.listdir('MIDI/'+sourceDir):
-			if not os.path.exists('channels/'+sourceDir+'/'+midiFile[:-4]):
-				print('    '+midiFile)
-				try:
-					splitChannels(midiFile,sourceDir)
-				except:
-					os.remove('MIDI/'+sourceDir+'/'+midiFile)
-					os.rmdir('channels/'+sourceDir+'/'+midiFile[:-4])
-					print('      --> REMOVED')
+	print("Scan a directory and add every channel of every MIDI file of this directory and its sub-directories to a list.")
